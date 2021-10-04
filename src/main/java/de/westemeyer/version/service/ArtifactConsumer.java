53c01acaa221e14e2aed2a66f8a1d67d4f9115ff
Sebastian Westemeyer <sebastian@westemeyer.de>
2021-10-04 21:19:21 +0200

    Updated imports and JavaDoc comment.

--- src/main/java/de/westemeyer/version/service/ArtifactConsumer.java
+++ src/main/java/de/westemeyer/version/service/ArtifactConsumer.java
@@ -2,8 +2,6 @@ package de.westemeyer.version.service;
 
 import de.westemeyer.version.model.Artifact;
 
-import java.util.Set;
-
 /**
  * An artifact consumer is a lambda (or object) which can be used when iterating list of artifacts to search for a
  * specific artifact.
@@ -11,7 +9,7 @@ import java.util.Set;
 @FunctionalInterface
 public interface ArtifactConsumer {
     /**
-     * Inspect the artifact content and use it (e.g. to put it in a {@link Set <Artifact>}) or return true when an artifact
+     * Inspect the artifact content and use it (e.g. to put it in a {@link java.util.Set}) or return true when an artifact
      * has been identified to get it returned immediately.
      *
      * @param artifact the artifact from service artifacts to be inspected by handler
